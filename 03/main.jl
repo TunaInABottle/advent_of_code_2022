0794575594f9191099663d7b631115ff77ce042a
TunaInABottle <social.tuna+github@pm.me>
2022-12-02 10:56:57 +0100

    added .gitignore

--- 03/main.jl
+++ 03/main.jl
@@ -1,8 +1,6 @@
-include("../utils.jl")
-using .Fileio
+using JSON, Requests
 
+first_request = get("https://adventofcode.com/2021/day/2/input")
+request_json = JSON.parse(first_request.data)
 
-
-raw_cont = read_txt("data.txt")
-
-println(raw_cont)
+println(request_json)
