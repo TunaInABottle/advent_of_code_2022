a39a8bb2ba1f602ae0f1b8cb878f1fcab818b1e5
TunaInABottle <social.tuna+github@pm.me>
2022-12-06 10:39:26 +0100

    Added typing to script

--- new_day.sh
+++ new_day.sh
@@ -25,13 +25,13 @@ touch example.txt
 sec_part() {
   local sec_n=${1:?Must provide an argument}
 
-  line_iter="solution = 0\n\nfor VAR_NAME = eachsplit(raw_cont, \"\\\\n\")\n\t#@TODO exercise\nend\n\n"
+  line_iter="solution::Int = 0\n\nfor VAR_NAME::String = eachsplit(raw_cont, \"\\\\n\")\n\t#@TODO exercise\nend\n\n"
   section="\n\n##################\n##### PART ${sec_n} #####\n##################\n${line_iter}println(\"part ${sec_n}: \$solution\")"
 #   return section
   echo -e $section
 }
 
 # preparing the .jl file with code that will surely be written
-echo -e "include(\"../utils.jl\")\nusing .Fileio\n\nraw_cont = read_txt(ARGS[1] * \".txt\")" >> main.jl
+echo -e "include(\"../utils.jl\")\nusing .Fileio\n\nraw_cont::String = read_txt(ARGS[1] * \".txt\")" >> main.jl
 sec_part 1 >> main.jl
 sec_part 2 >> main.jl
